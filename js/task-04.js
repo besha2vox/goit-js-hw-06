4288fd12481a15f08a04602dd4206f4729fe887b
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-10-25 23:02:57 +0300

    complete task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,18 @@
 let counterValue = 0;
+
+const decrementRef = document.querySelector('[data-action="decrement"]');
+console.table('decrementRef', decrementRef);
+const incrementRef = document.querySelector('[data-action="increment"]');
+const valueRef = document.querySelector('#value');
+
+const decrementValue = () => {
+    counterValue -= 1;
+    valueRef.textContent = counterValue;
+};
+const incrementValue = () => {
+    counterValue += 1;
+    valueRef.textContent = counterValue;
+};
+
+decrementRef.addEventListener('click', decrementValue);
+incrementRef.addEventListener('click', incrementValue);
