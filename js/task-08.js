26a046274099d07a458c50c94d8f5cd3a59c5237
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-10-26 02:28:56 +0300

    complete task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginFormRef = document.querySelector('.login-form');
 
+const loginFormSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Усі поля мають бути заповнені');
+  }
+  console.log(`${email.name}: ${email.value}, ${password.name}: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+loginFormRef.addEventListener('submit', loginFormSubmit);
