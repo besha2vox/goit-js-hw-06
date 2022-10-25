57f678db1908922977207d1edc8c572076bc3527
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-10-26 01:22:51 +0300

    complete task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const textRef = document.querySelector('.color');
+
+const changeColor = () => {
+  const color = getRandomHexColor();
+  textRef.textContent = color;
+  document.body.style.backgroundColor = color;
+};
+
+btnRef.addEventListener('click', changeColor);
