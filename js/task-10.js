8eb641ec644478ad90540d333d9487da7af17e26
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-10-26 03:04:04 +0300

    complete task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputRef = document.querySelector('#controls input');
+const createBtnRef = document.querySelector('[data-create]');
+const destroyBtnRef = document.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+let size = 0;
+const elements = [];
+
+const makeDiv = () => {
+  const amountOfElements = Number(inputRef.value);
+
+  for (let i = 0; i < amountOfElements; i += 1) {
+    const divBox = document.createElement('div');
+    size += size > 0 ? 10 : 30;
+    divBox.style.width = `${size}px`;
+    divBox.style.height = divBox.style.width;
+    divBox.style.backgroundColor = getRandomHexColor();
+    elements.push(divBox);
+  }
+  boxesRef.append(...elements);
+};
+
+const delDiv = () => {
+  boxesRef.innerHTML = '';
+};
+
+createBtnRef.addEventListener('click', makeDiv);
+destroyBtnRef.addEventListener('click', delDiv);
